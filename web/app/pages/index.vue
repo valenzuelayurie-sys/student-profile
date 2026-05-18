b363fa87d3a75da897d4b2cd476d9dda0e87b560
valenzuelayurie-sys <valenzuelayurie@gmail.com>
2026-05-18 10:22:42 +0800

    cms and web integrate

--- web/app/pages/index.vue
+++ web/app/pages/index.vue
@@ -9,6 +9,7 @@
               >
                   <v-text-field
                   label=" STUDENT NO "
+                  prepend-inner-icon="mdi mdi-account-circle"
                   variant="solo-inverted"
                   placeholder="24-00611">
                 </v-text-field>
@@ -19,6 +20,7 @@
               md="4">
                   <v-text-field
                   label=" LAST NAME "
+                  prepend-inner-icon="mdi mdi-account-box-outline"
                   variant="solo-inverted"
                   placeholder="VALENZUELA">
                 </v-text-field>
@@ -29,6 +31,7 @@
               md="4">
                   <v-text-field
                   label=" FIRST NAME "
+                  prepend-inner-icon="mdi mdi-account-box-outline"
                   variant="solo-inverted"
                   placeholder="YURIE">
                 </v-text-field>
@@ -39,6 +42,7 @@
               md="4">
                 <v-text-field
                   label=" MIDDLE NAME "
+                  prepend-inner-icon="mdi mdi-account-box-outline"
                   variant="solo-inverted"
                   placeholder="BAYA">
                 </v-text-field>
@@ -49,6 +53,7 @@
               md="4">
                 <v-combobox
                   label=" COURSE "
+                  prepend-inner-icon="mdi mdi-account-details-outline"
                   variant="outlined"
                   :items="['BSCS','BSIT']"
                   clearable>
@@ -60,6 +65,7 @@
               md="4">
                 <v-combobox
                   label=" YEAR "
+                  prepend-inner-icon="mdi mdi-account-details-outline"
                   variant="outlined"
                   :items="['1st year','2nd year','3rd year','4th year']"
                   clearable>
@@ -71,6 +77,7 @@
               md="4">
                 <v-combobox
                   label=" SECTION "
+                  prepend-inner-icon="mdi mdi-account-details-outline"
                   variant="outlined"
                   :items="['A','B','C']"
                   clearable>
@@ -82,6 +89,7 @@
               md="4">
                 <v-text-field
                   label=" ADDRESS "
+                  prepend-inner-icon="mdi mdi-map-marker-account"
                   variant="solo-inverted"
                   placeholder="BARANGAY SAN AGUSTIN">
                 </v-text-field>
@@ -92,6 +100,7 @@
               md="4">
                 <v-text-field
                   label=" CONTACT No, "
+                  prepend-inner-icon="mdi mdi-card-account-details"
                   variant="solo-inverted"
                   placeholder="0921241547">
                 </v-text-field>
@@ -102,6 +111,7 @@
               md="4">
                 <v-combobox
                   label=" GENDER "
+                  prepend-inner-icon="mdi mdi-gender-transgender"
                   variant="outlined"
                   :items="['MALE','FENALE','LGBTQ+A']"
                   clearable>
@@ -138,9 +148,45 @@
               >DELETE
 
             </v-btn>
+
+             <v-card
+              title="Student Profile"
+              flat
+            >
+              <template v-slot:text>
+                <v-text-field
+                  v-model="hanapstudent"
+                  label="Search"
+                  prepend-inner-icon="mdi-magnify"
+                  variant="outlined"
+                  hide-details
+                  single-line
+                ></v-text-field>
+              </template>
+
+              <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :search="hanapstudent"
+              ></v-data-table>
+            </v-card>
+
+
+
         </v-container>
       </v-main>
     </v-app>
   </template>
+
+    <script setup>
+    const hanapstudent = ref('')
+
+    const headers = [
+      {
+        key: 'studno' , tittle: 'student number'
+      }
+      
+    ]
+  </script>
 
   
